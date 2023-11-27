5684243f508809adae71c82b42501b9a3b2ddc84
Ricardo Robles <rroblesgo@gmail.com>
2023-11-27 21:11:24 +0100

    Build a Multistep Form in NextJs Using react-hook-form and ZOD

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,19 @@
 // import Rhf from '@/components/rhf'
 // import SimpleForm from '@/components/simple'
 // import RhfWithZod from '@/components/rhf-with-zod'
-import RhfWithAction from '@/components/with-action'
+// import RhfWithAction from '@/components/with-action'
+import Form from '@/components/form'
 
 export default function Home() {
   return (
     <section className='py-24'>
       <div className='container'>
-        <h1 className='mb-16 text-2xl font-medium'>React Hook Forms</h1>
+        {/* <h1 className='mb-16 text-2xl font-medium'>React Hook Forms</h1> */}
         {/* <SimpleForm /> */}
         {/* <Rhf /> */}
         {/* <RhfWithZod /> */}
-        <RhfWithAction />
+        {/* <RhfWithAction /> */}
+        <Form />
       </div>
     </section>
   )
--- lib/schema.ts
+++ lib/schema.ts
@@ -7,3 +7,14 @@ export const FormDataSchema = z.object({
     .nonempty('Message is required.')
     .min(6, { message: 'Message must be at least 6 characters.' })
 })
+
+export const FormDataSchema2 = z.object({
+  firstName: z.string().min(1, 'First name is required'),
+  lastName: z.string().min(1, 'Last name is required'),
+  email: z.string().min(1, 'Email is required').email('Invalid email address'),
+  country: z.string().min(1, 'Country is required'),
+  street: z.string().min(1, 'Street is required'),
+  city: z.string().min(1, 'City is required'),
+  state: z.string().min(1, 'State is required'),
+  zip: z.string().min(1, 'Zip is required')
+})
