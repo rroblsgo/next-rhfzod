0fb145a0275cfa488f5980c8784744f46d761b07
Ricardo Robles <rroblesgo@gmail.com>
2023-11-27 18:53:56 +0100

    Adding Zod validation

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
-import Rhf from '@/components/rfh'
+// import Rhf from '@/components/rhf'
 // import SimpleForm from '@/components/simple'
+import RhfWithZod from '@/components/rhf-with-zod'
+
 export default function Home() {
   return (
     <section className='py-24'>
       <div className='container'>
         <h1 className='mb-16 text-2xl font-medium'>React Hook Forms</h1>
         {/* <SimpleForm /> */}
-        <Rhf />
-        {/* <RhfWithZod /> */}
+        {/* <Rhf /> */}
+        <RhfWithZod />
         {/* <RhfWithAction /> */}
       </div>
     </section>
