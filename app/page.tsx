f5f531eb7e210d02936462973e398bbea6494737
Ricardo Robles <rroblesgo@gmail.com>
2023-11-27 18:29:34 +0100

    Adding React hook form

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-import SimpleForm from '@/components/simple'
+import Rhf from '@/components/rfh'
+// import SimpleForm from '@/components/simple'
 export default function Home() {
   return (
     <section className='py-24'>
       <div className='container'>
         <h1 className='mb-16 text-2xl font-medium'>React Hook Forms</h1>
-        <SimpleForm />
-        {/* <Rhf /> */}
+        {/* <SimpleForm /> */}
+        <Rhf />
         {/* <RhfWithZod /> */}
         {/* <RhfWithAction /> */}
       </div>
